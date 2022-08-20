6d819efe62f557002ba3173de90dc13ff6f941a7
Muziba Shaikh <muzibashaikh@gmail.com>
2022-08-20 22:09:35 +0530

    added function to add two numbers

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+
+function addNumbers(a, b) {
+    const sum = a + b;
+    console.log("sum:", sum)
+    return sum;
+}
+
+addNumbers(5, 8);
